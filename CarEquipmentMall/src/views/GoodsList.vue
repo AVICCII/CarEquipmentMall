6b2fccdc3c2350b19fb12814b003eb2e283bbfcd
AVICCII <863134471@qq.com>
2020-11-10 17:53:30 +0800

    2020/11/10

---
+++ CarEquipmentMall/src/views/GoodsList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      这是商品列表页面
+      <span>{{$route.params.goodsId}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GoodsList"
+    }
+</script>
+
+<style >
+
+</style>
